3a1d2a3799698552ec049e51286edfd0f3cdd9f9
KayoSavio <kayosavio@hotmail.com>
2020-03-20 20:44:54 -0300

    Conectando ao DataBase

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const server = express();
+
+mongoose.connect('mongodb://localhost:27017/products',{ useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if(err){
+    console.log('Erro em concetar ao DB');
+  }console.log('Conseguiu se conectar!');
+});
 
 server.use('/',(req,res) => {
   res.send('Server Funcionando');
 })
 
-server.listen(8000);
+const port = 3000;
+server.listen(port,(err) => {
+  if(err){
+  return console.log('Não foi possivel acessar o servidor!');
+  }console.log('Conectado em Https://localhost:8000');
+});
